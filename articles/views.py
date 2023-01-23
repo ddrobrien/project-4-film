e18f5b60e766032c6f7791b45b69a7f3c2452289
David O'Brien <ddrobrien@icloud.com>
2023-01-23 16:05:52 +0000

    changes to index.html to include pagination, will urls and views linked up

--- articles/views.py
+++ articles/views.py
@@ -6,10 +6,12 @@ from .forms import CommentForm
 
 
 class PostList(generic.ListView):
+    model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 3
 
-
+ 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
