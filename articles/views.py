051d99aad78933acf03624368b04282ef5dbe069
David O'Brien <ddrobrien@icloud.com>
2023-01-25 11:31:57 +0000

    comments section created and working

--- articles/views.py
+++ articles/views.py
@@ -11,36 +11,57 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 3
 
- 
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-    def post_detail(request, slug):
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
-        template_name = 'post_detail.html'
-        post = get_object_or_404(Post, slug=slug)
-        comments = Comment.objects.filter(active=True)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        new_comment = None
-    # Comment posted
-        if request.method == 'POST':
-            comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
 
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.post = post
-            # Save the comment to the database
-            new_comment.save()
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
         else:
             comment_form = CommentForm()
 
-        return render(request, "post_detail.html", {
-            'post': post,
-            'comments': comments,
-            'liked': liked,
-            'new_comment': new_comment,
-            'comment_form': comment_form})
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
--- film/settings.py
+++ film/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django.contrib.staticfiles',
     'django_summernote',
+    'crispy_forms',
     'articles',
 ]
 
@@ -62,6 +63,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ROOT_URLCONF = 'film.urls'
 
