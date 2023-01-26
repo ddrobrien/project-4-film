4d16b9debc2d64b260512bcadb639b5fb8e15a63
David O'Brien <ddrobrien@icloud.com>
2023-01-26 13:36:49 +0000

    heroku deployment commit

--- film/settings.py
+++ film/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('milliepuppy'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-4-film-review.herokuapp.com', 'localhost']
 
 
 # Application definition
